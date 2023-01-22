6fd7f3fa4f124668672a1021bd38ff09b0d71153
Ibrahim Abdi <ibrahimabdi2367@gmail.com>
2023-01-22 15:54:40 -0500

    net ninja javascripet

---
+++ test.js
@@ -0,0 +1,29 @@
+// // // // // the first alert
+// // // // alert("Hello World");
+
+// // // // //second alert
+// // // // alert("Hello World");
+// // // // /* bbbbbbbbbbbbbbbbbb
+// // // // bbbbbbbb
+// // // // bbbbbbbbb */
+
+// // // var myVarialbeAbdi
+
+// // // myVarialbeAbdi = 10;
+
+// // // var myVar2 = "hello"
+// // // myVar2 = "hello again"
+
+// // // myVar2 = 5;
+
+// // var myVar = 5;
+
+// // myVar
+// // 5+5
+// // myVar + 10;
+// //  myVar = myVar + 10;
+
+// var myVar = "Abdi"
+// document.write(myVar);
+// document.write(10);
+// console.log(50);
